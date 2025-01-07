28ecad50d4594e6511537491f61cb9b8226e49da
Ratchanont Thippimanporn <137043533+Ratchanontt@users.noreply.github.com>
2025-01-07 12:27:11 +0700

    Add files via upload

--- myfirstpy.py
+++ myfirstpy.py
@@ -1,21 +1,23 @@
-import argparse 
+import argparse
 from datetime import datetime
 
 def parse_input():
     parser = argparse.ArgumentParser()
 
+    def parse_date(date_str):
+        return datetime.strptime(date_str, '%d/%m/%Y')
+
     parser.add_argument(
         '--bd',
-        type=int,
+        type=parse_date,
         required=True,
-        help='birthday of the user'
+        help='Birthday of the user in format DD/MM/YYYY'
     )
     parser.add_argument(
         '--name',
         type=str,
         default='Ratchanont',
-        # required=True,
-        help='input the name of people who are using the app'
+        help='Input the name of the person using the app'
     )
 
     args = parser.parse_args()
@@ -25,11 +27,19 @@ def printHello(who):
     print(f"Hello World, {who}!!")
 
 def cal_todayVbd(bd):
-    td = datetime.today().strftime('%d')
-    return bd-int(td)
+    today = datetime.today()
+    delta = bd - today
+    return delta.days
 
-if __name__== "__main__":
+if __name__ == "__main__":
     input_v = parse_input()
     print('This is my first .py file.')
     printHello(input_v.name)
-    print(f'your birthday is {cal_todayVbd(input_v.bd)} from today')
+    
+    days_to_birthday = cal_todayVbd(input_v.bd)
+    if days_to_birthday > 0:
+        print(f'Your birthday is in {days_to_birthday} day(s) from today.')
+    elif days_to_birthday == 0:
+        print("Happy Birthday!")
+    else:
+        print(f'You have lived for {-days_to_birthday} day(s).')
